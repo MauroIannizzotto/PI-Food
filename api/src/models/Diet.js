89b1b4fa45ee76416fd17cc55c3f341a7f9334c0
MauroIannizzotto <mauro.iannizzotto@hotmail.com>
2022-05-02 10:39:10 -0300

    Estructura Lista

--- api/src/models/Diet.js
+++ api/src/models/Diet.js
@@ -3,32 +3,14 @@ const { DataTypes } = require("sequelize");
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define("recipe", {
+  sequelize.define("diet", {
     title: {
       type: DataTypes.STRING,
-      allowNull: false,
     },
     id: {
       type: DataTypes.UUID, //uuid para que no se pise con el id de la api
       defaultValue: DataTypes.UUIDV4,
-      allowNull: false,
       primaryKey: true, //va a ser la clave primaria
-    },
-    summary: {
-      type: DataTypes.TEXT,
-      allowNull: false
-    },
-    spoonacularScore: {
-      type: DataTypes.INTEGER
-    },
-    healthScore: {
-      type: DataTypes.INTEGER
-    },
-    steps: {
-      type: DataTypes.ARRAY(DataTypes.TEXT)
-    },
-    image: {
-      type: DataTypes.TEXT
     }
   });
 };
